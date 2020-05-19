452e50420445977420ffe886e85255dc13cd0a58
kianenigma <kian.peymani@gmail.com>
2020-05-19 19:13:30 +0200

    more playground stuff

--- src/subcommands/playground.rs
+++ src/subcommands/playground.rs
@@ -1,35 +1,58 @@
-use crate::primitives::{AccountId, Balance, Index};
+use crate::primitives::{AccountId, Balance, Hash};
 use crate::{network, storage, Client, CommonConfig};
 use codec::Encode;
 use frame_system::AccountInfo;
+use kusama_runtime::{Call, UncheckedExtrinsic};
 use pallet_balances::AccountData;
+use pallet_staking::Call as StakingCall;
 
 #[allow(unused)]
-pub async fn run(client: &Client, _: CommonConfig) {
-	let account: AccountId =
-		hex_literal::hex!["da410f5e2c6e1e29bd53dd81f864380e8bcdf4bd8d8a84f2c5f3897890893452"]
-			.into();
-
-	let block_hash_before = network::get_head(client).await;
-	let info_at: AccountInfo<Index, AccountData<Balance>> = storage::read(
-		storage::map_key::<frame_support::Blake2_128Concat>(
-			b"System",
-			b"Account",
-			account.as_ref(),
-		),
+pub async fn run(client: &Client, command_config: CommonConfig) {
+	let account =
+		hex_literal::hex!["2c61f078a240b295eb8d19db50e5b27b39225ede1cf718c0872c441cb7ac8d54"];
+	let account_data = storage::read::<AccountInfo<u32, AccountData<Balance>>>(
+		storage::map_key::<frame_support::Blake2_128Concat>(b"System", b"Account", &account),
 		client,
-		block_hash_before,
+		command_config.at,
 	)
-	.await
-	.unwrap();
+	.await;
 
-	let account_data = info_at.data;
+	dbg!(account_data);
+}
 
-	let latest = network::get_head(client).await;
-	let block = network::get_block(client, latest).await;
-	// dbg!(&block);
-	for e in block.block.extrinsics {
-		let info = network::get_xt_info(client, e.encode().into(), latest).await;
-		dbg!(info);
+async fn find_defunct_nominators(client: &Client, common_config: CommonConfig) {
+	//
+}
+
+async fn find_defunct_voter(client: &Client, common_config: CommonConfig) {
+	// read all current members
+	// read all current runners
+	// read all current candidates
+	// read Voting of target
+	// Search if any
+}
+
+async fn last_election_submission(client: &Client) {
+	let mut now = network::get_head(client).await;
+
+	loop {
+		let block = network::get_block(client, now).await.block;
+		let parent_hash = block.header.parent_hash;
+		println!(
+			"Checking {}. {} extrinsics",
+			block.header.number,
+			block.extrinsics.len(),
+		);
+		for e in block.extrinsics {
+			let call: Call = e.clone().function;
+			match call {
+				Call::Staking(staking_call) => {
+					dbg!(&staking_call);
+				}
+				_ => {}
+			}
+		}
+
+		now = parent_hash;
 	}
 }
